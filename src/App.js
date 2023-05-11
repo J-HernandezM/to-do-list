683325b905c529e010fce87bc64adbf0d40e11a4
Juan <juhernandezm@unal.edu.co>
2023-05-11 15:47:22 -0500

    clase 09 y 10: feature complete, delete, uncomplete y search

--- src/App.js
+++ src/App.js
@@ -8,17 +8,22 @@ import { CreateTodoButton } from './CreateTodoButton';
 const defaultTodos = [
   {text:'Cortar cebolla', completed: true},
   {text:'Montar Caballo', completed: false},
-  {text:'Rotar pepino', completed: false},
+  {text:'Rotar manzanas', completed: false},
   {text:'Peinar peludos', completed: true},
+  {text:'Coortar cebolla', completed: true},
+  {text:'Moontar Caballo', completed: false},
+  {text:'Rootar manzanas', completed: false},
+  {text:'Peeinar peludos', completed: true},
 ]
 
 function App() {
   const [todos, setTodos] = React.useState(defaultTodos)
   const [searchValue, setSearchValue] = React.useState('')
-  //lenth de un nuevo array que contenga los todos con completed true
+  //Contando todos
+  //length de un nuevo array que contenga los todos con completed true
   const completedTodos = todos.filter(todo=>todo.completed).length
   const totalTodos = todos.length
-
+  //Buscando todos
   const found = todos.filter((todo)=>{
     if(searchValue!=''){
       if(todo.text.toLowerCase().includes(searchValue.toLowerCase())){
@@ -26,6 +31,7 @@ function App() {
       }
     }else{return todo}
   })
+
   return (
     <>
       <TodoCounter total={totalTodos} completed={completedTodos}/> 
@@ -39,7 +45,9 @@ function App() {
 
       <TodoList>
         {found.map(todo=>{
-          return(<TodoItem 
+          return(<TodoItem
+            setTodos={setTodos}
+            todos={todos} 
             key={todo.text} 
             text={todo.text} 
             completed={todo.completed}/>)
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,16 +1,55 @@
 import './TodoItem.css'
 function TodoItem(props){
+  function checkTodo(checkThis){
+    const updatedTodos=[]
+    props.todos.forEach((todo)=>{
+      if(todo.text===checkThis){
+        todo.completed=true
+        updatedTodos.push(todo)
+      }else{
+        updatedTodos.push(todo)
+      }
+    })
+    props.setTodos(updatedTodos)
+  }
+  function  uncheckTodo(uncheckThis){
+    const updatedTodos=[]
+    props.todos.forEach((todo)=>{
+      if(todo.text===uncheckThis){
+        todo.completed=false
+        updatedTodos.push(todo)
+      }else{
+        updatedTodos.push(todo)
+      }
+    })
+    props.setTodos(updatedTodos)
+  }
+  function  deleteTodo(deleteThis){
+    const updatedTodos=[]
+    props.todos.forEach((todo)=>{
+      if(todo.text!==deleteThis){
+        updatedTodos.push(todo)
+      }
+    })
+    props.setTodos(updatedTodos)
+  }
     return(
       <li className='todo'>
         <span className={`fa-regular fa-circle-check fa-lg icons check ${props.completed?"inactive":"check"}`} onClick={(event)=>{
-          console.log('Quieres completar un todo');
+          const message = event.target.parentElement.childNodes[2].textContent
+          console.log('completar');
+          checkTodo(message)
         }}></span>
         <span className={`fa-solid fa-circle-check fa-lg icons ${props.completed?"checked":"inactive"}`} style={{color: '#0268ee'}} onClick={(event)=>{
-          console.log('Quieres descompletar un todo');
+          const message = event.target.parentElement.childNodes[2].textContent
+          console.log('descompletar');
+          uncheckTodo(message)
         }}></span>
         <p className={`${props.completed?"task--completed":"task"}`}>{props.text}</p>
         <span className="fa-regular fa-circle-xmark fa-lg icons close" onClick={(event)=>{
-          console.log('Quieres eliminar un todo');
+          const message = event.target.parentElement.childNodes[2].textContent
+          console.log('eliminar');
+          deleteTodo(message)
         }}></span>
       </li>
     )
