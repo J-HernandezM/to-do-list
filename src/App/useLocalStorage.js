80800a51dccd3425941042b27ff875e2bfe45d38
Juan <juhernandezm@unal.edu.co>
2023-06-28 15:52:38 -0500

    manejo del estado e implementacion de reducer en localStorage

--- src/App/useLocalStorage.js
+++ src/App/useLocalStorage.js
@@ -1,12 +1,22 @@
 import React from 'react';
 //CUSTOM HOOK, ABSTRAER LOGICA PARA ALIVIANAR LA COMPLEJIDAD DEL COMPONENTE
 function useLocalStorage(itemName, defaultItem){
-    const [synchronized, setSynchronized] = React.useState(true)
-    //creamos un nuevo estado
-    const [items, setItems] = React.useState(defaultItem)
-    //estado de carga y error
-    const [loading, setLoading] = React.useState(true)
-    const [error, setError] = React.useState(false)
+//Implementacion reducer
+const [state, dispatch] = React.useReducer(reducer, initialState(defaultItem))
+
+const {
+  synchronized,
+  items,
+  loading,
+  error} = state
+
+//Action creators
+const onError = (error)=>dispatch({type:actionTypes.error, payload: error})
+const onSuccess = ()=>dispatch({type:actionTypes.sucess})
+const onSynchronize = ()=>dispatch({type:actionTypes.synchronize})
+const onSave = (item)=>dispatch({type:actionTypes.save, payload: item})
+
+
     //aplicamos la misma logica que teniamos antes
     React.useEffect(()=>{
       setTimeout(()=>{
@@ -18,16 +28,11 @@ function useLocalStorage(itemName, defaultItem){
             parsedItem=defaultItem
           }else{
             parsedItem=JSON.parse(storageItem)
-            setItems(parsedItem)
+            onSave(parsedItem)
           }
-          setLoading(false)
-
-          setSynchronized(true)
-
+          onSuccess()
         }catch(error){
-          console.log(error);
-          setLoading(false)
-          setError(true)
+          onError(error)
         }}, 2000)
         // eslint-disable-next-line
       }, [synchronized])
@@ -35,18 +40,67 @@ function useLocalStorage(itemName, defaultItem){
     function saveItem(newItemArray){
       const stringedArray = JSON.stringify(newItemArray)
       localStorage.setItem(itemName, stringedArray)
-      setItems(newItemArray)
+      onSave(newItemArray)
     }
 
     //Funcion de sincronizar, recargar
     function synchronize(){
-      setLoading(true)
-      setSynchronized(false)
+      onSynchronize()
     }
-
-
     //retornamos ese estado y esa forma de actualizar el estado
     return {items, saveItem, loading, error, synchronize}
+}
+
+const initialState = (initialValue)=> {
+  return({
+    synchronized:true,
+    items: initialValue,
+    loading:true,
+    error: false
+  })
+}
+
+const actionTypes = {
+  error: 'ERROR',
+  success: 'SUCCESS',
+  save: 'SAVE',
+  save2: 'SAVE2',
+  synchronize: 'SYNCHRONIZE'
+}
+
+const reducerObject = (state, payload) =>{
+  return({
+    [actionTypes.error]: {
+      ...state, 
+      error: true,
+      loading: false
+    },
+    [actionTypes.success]: {
+      ...state, 
+      synchronized: true,
+      loading: false
+    },
+    [actionTypes.synchronize]:{
+      ...state,
+      loading: true,
+      synchronized: false
+    },
+    [actionTypes.save]:{
+      ...state,
+      items: payload,
+      loading: false,
+      synchronized: true
+    }
+  })
+
+}
+
+const reducer = (state, action)=>{
+  if(reducerObject(state)[action.type]){
+      return reducerObject(state, action.payload)[action.type]
+  }else{
+      return state
+  }
 }
 
 export {useLocalStorage}
