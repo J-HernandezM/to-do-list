4753e33b57c37bb5eb73310bedad809fb6632c65
Juan <juhernandezm@unal.edu.co>
2023-09-04 10:55:30 -0500

    feat: searchParams implemented in the slug

--- src/UI/TodoSearch/index.js
+++ src/UI/TodoSearch/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './TodoSearch.css'
+import { useSearchParams } from 'react-router-dom';
 function TodoSearch({searchValue, setSearchValue}){
+    let [searchParams, setSearchParams] = useSearchParams();
     return(
         <input placeholder="Busca un to-do" className="searchbar todoInput" value={searchValue} onChange={(event)=>{
             setSearchValue(event.target.value)
+            setSearchParams({search: event.target.value})
         }}/>
     )
 }
